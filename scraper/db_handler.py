252a85f4e8c7931d57d91c4ab0bf0f607e753a03
FirminoEduardo <dudu.lf@outlook.com>
2025-01-03 06:26:13 -0300

    Configuracao de dependencias, banco de dados e ponto de entrada

--- scraper/db_handler.py
+++ scraper/db_handler.py
@@ -1,1 +1,21 @@
 # Funções para interagir com o MongoDB
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+# Carregar variavéis de ambiente
+load_dotenv()
+
+# Configuração do mongoDB
+client = MongoClient(os.getenv("MONGO_URI"))
+db = client[os.getenv("DATABASE_NAME")]
+
+def save_product(data, collection_name="products"):
+    """Salva um produto no MongoDB"""
+    collection = db[collection_name]
+    collection.insert_one(data)
+
+def get_products(collection_name="products"):
+    """Recupera todos os produtos"""
+    collection = db[collection_name]
+    return list(collection_name.find())
